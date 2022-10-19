97f29f874b70b2548d6c9bb4c402b1fd0b46736b
khellud <khalidhamid993@gmail.com>
2022-10-19 21:31:31 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const XylaphoneApp());
@@ -13,7 +14,14 @@ class XylaphoneApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioCache();
+              },
+              child: const Text('Click me'),
+            ),
+          ),
         ),
       ),
     );
