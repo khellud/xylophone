7cc256499e87c4c3ad1cf7eca840c8355c545e4d
khellud <khalidhamid993@gmail.com>
2022-11-17 14:31:12 +0300

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,10 @@ class XylaphoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.green, soundNumber: 1),
+              buildKey(color: Colors.yellow, soundNumber: 2),
+              buildKey(color: Colors.red, soundNumber: 3),
+              buildKey(color: Colors.black, soundNumber: 4),
               buildKey(color: Colors.teal, soundNumber: 5),
               buildKey(color: Colors.blue, soundNumber: 6),
               buildKey(color: Colors.purple, soundNumber: 7),
